a522bec6e9a7079c8b381cf0d4c1546ef5f6f123
peacelovephysics <43553606+peacelovephysics@users.noreply.github.com>
2018-09-24 14:17:56 -0600

    add hello world
    this added hello world!

---
+++ class.m
@@ -0,0 +1,12 @@
+classdef class
+    %CLASS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ test.m
@@ -0,0 +1,1 @@
+fprint("Hello world.")
